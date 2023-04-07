ebb6d76ef6e25db217a644bd4ab61f47b539abe1
Adnan-Khan <107304882+VYcanis-majoris@users.noreply.github.com>
2023-04-07 18:26:10 +0500

    Update simple-interest.sh

--- simple-interest.sh
+++ simple-interest.sh
@@ -5,8 +5,8 @@
    # Do not use this in production. Sample purpose only.
 
    # Author: Upkar Lidder (IBM)
-   # Additional Authors:
-   # <your GitHub username>
+   # Additional Authors: Adnan Khan
+   # Adnan-Khan
 
    # Input:
    # p, principal amount
